88f8f5cb05a02e38193aec5d4c4b40ed0795ac1c
Andrew <andrewboudreau@gmail.com>
2016-09-28 08:23:47 -0500

    more javascript api, cascade delete

---
+++ src/Groceries.Boudreau.Cloud.WebApp/Migrations/20160928055031_nc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Groceries.Boudreau.Cloud.WebApp.Migrations
+{
+    public partial class nc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingItems_ShoppingLists_ShoppingListId",
+                table: "ShoppingItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingItems_ShoppingLists_ShoppingListId",
+                table: "ShoppingItems",
+                column: "ShoppingListId",
+                principalTable: "ShoppingLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingItems_ShoppingLists_ShoppingListId",
+                table: "ShoppingItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingItems_ShoppingLists_ShoppingListId",
+                table: "ShoppingItems",
+                column: "ShoppingListId",
+                principalTable: "ShoppingLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- src/Groceries.Boudreau.Cloud.WebApp/wwwroot/js/site.js
+++ src/Groceries.Boudreau.Cloud.WebApp/wwwroot/js/site.js
@@ -1,12 +1,65 @@
 ﻿// Write your Javascript code.
 groceries = {
-    api: {
-        get: function (id) {
-            if (id) {
-                return $.get("/api/shoppinglist/" + id);
-
+    models: {
+        ShoppingList: function () {
+            return {
+                id: 0,
+                name: "",
+                items: []
             }
-            return $.get("/api/shoppinglist/");
         },
+        ShoppingItem: function () {
+            return {
+                id: 0,
+                name: "",
+                quantity: 1,
+                isChecked: false
+            }
+        }
+    },
+    api: {
+        shoppinglist: {
+            get: function (id) {
+                if (id) {
+                    return $.get("/api/shoppinglist/" + id);
+                }
+                return $.get("/api/shoppinglist/");
+            },
+            post: function (shoppinglist, done, fail) {
+                return $.ajax("/api/shoppinglist", {
+                    method: "POST",
+                    contentType: 'application/json; charset=UTF-8',
+                    dataType: "json",
+                    data: JSON.stringify(shoppinglist)
+                });
+            },
+            put: function (shoppinglist, done, fail) {
+                return $.ajax("/api/shoppinglist", {
+                    method: "PUT",
+                    contentType: 'application/json; charset=UTF-8',
+                    dataType: "json",
+                    data: JSON.stringify({ id: shoppinglist.id, shoppinglist })
+                });
+            },
+            delete: function (id) {
+                return $.ajax("/api/shoppinglist/" + id, {
+                    method: "DELETE"
+                });
+            }
+        }
     }
 }
+
+var list = new groceries.models.ShoppingList();
+list.items.push(new groceries.models.ShoppingItem());
+list.items[0].name = "item1234";
+list.name = "list1234";
+
+var done = function () {
+
+    groceries.api.shoppinglist.get().done(function () { console.log(arguments[0]); });
+}
+
+groceries.api.shoppinglist.post(list).done(done);
+
+
