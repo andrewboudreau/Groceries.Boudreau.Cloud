8a0952e43a411ab5c7e2de2d4a64e21d1ca649d4
Andrew <andrewboudreau@gmail.com>
2016-10-13 23:56:58 -0500

    adding angular directives

--- src/Groceries.Boudreau.Cloud.WebApp/wwwroot/app/app.component.ts
+++ src/Groceries.Boudreau.Cloud.WebApp/wwwroot/app/app.component.ts
@@ -1,7 +1,43 @@
 ﻿import { Component } from '@angular/core';
 
+
+const SHOPPINGLISTS: ShoppingList[] = [
+    { id: 11, name: 'Test Shopping List One' },
+    { id: 12, name: 'Test List 2' }
+];
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+    <h2>My Shopping Lists</h2>
+    <ul class="lists">
+        <li *ngFor="let list of shoppingLists" (click)="onSelect(list)">
+            <span class="badge">{{list.id}}</span> {{list.name}}
+        </li>
+    </ul>
+    <div *ngIf="selectedShoppingList">
+      <h2>{{selectedShoppingList.name}} details!</h2>
+      <div><label>id: </label>{{selectedShoppingList.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedShoppingList.name" placeholder="name"/>
+      </div>
+    </div>
+  `
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Groceries Lists';
+    shoppingLists = SHOPPINGLISTS;
+    selectedShoppingList: ShoppingList;
+    onSelect(shoppinglist: ShoppingList): void {
+        this.selectedShoppingList = shoppinglist;
+    }
+
+}
+
+export class ShoppingList {
+    id: number;
+    name: string;
+}
